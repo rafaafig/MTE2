dd9ad36a43a9e21ffd88403621b7c5da14b9a27e
Francisco Maia <franciscomaia097@gmail.com>
2023-12-20 14:49:44 +0000

    Adicionado Point class

---
+++ src/main/java/org/example/Point.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Point {
+
+   private int coordinate;
+
+    public int getCoordinate() {
+        return coordinate;
+    }
+
+    public void setCoordinate(int coordinate) {
+        this.coordinate = coordinate;
+    }
+}
