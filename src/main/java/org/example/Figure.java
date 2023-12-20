d81308b2a9c9ff6b3334cb936bb224389867e29a
Rafael <rafa.figueiredo48@gmail.com>
2023-12-20 14:55:54 +0000

    Added get and set to Color

--- src/main/java/org/example/Figure.java
+++ src/main/java/org/example/Figure.java
@@ -8,4 +8,12 @@ public abstract class Figure {
         this.color=color;
     }
 
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
 }
