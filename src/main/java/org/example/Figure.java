c35ef5df8293e427ce544feaa7438f3244e725cc
Rafael <rafa.figueiredo48@gmail.com>
2023-12-20 14:46:20 +0000

    Add Color attribute

--- src/main/java/org/example/Figure.java
+++ src/main/java/org/example/Figure.java
@@ -1,2 +1,11 @@
-package org.example;public class Figure {
+package org.example;
+
+public abstract class Figure {
+
+    private String color;
+
+    public Figure (String color){
+        this.color=color;
+    }
+
 }
