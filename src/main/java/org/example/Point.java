ad80e4c1591a5b9c64fb71af0234daa52d4a63a2
Pedro Costa <costapm96@gmail.com>
2023-12-20 14:55:52 +0000

    Alteraçao da classe Point

--- src/main/java/org/example/Point.java
+++ src/main/java/org/example/Point.java
@@ -2,13 +2,34 @@ package org.example;
 
 public class Point {
 
-   private int coordinate;
+    private double x;
+    private double y;
 
-    public int getCoordinate() {
-        return coordinate;
+    // Constructors
+    public Point() {
+        this(0.0, 0.0);
     }
 
-    public void setCoordinate(int coordinate) {
-        this.coordinate = coordinate;
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getter and Setter methods for x
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    // Getter and Setter methods for y
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
     }
 }
